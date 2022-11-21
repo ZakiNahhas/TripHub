bd0cc61cf6288d00d20a885034b77ac3fd9d9b1b
Zaki <zakinahhas97@outlook.com>
2022-11-21 16:02:59 +0200

    fixed guide model

--- src/main/java/com/project/TripHub/models/Guide.java
+++ src/main/java/com/project/TripHub/models/Guide.java
@@ -1,27 +1,97 @@
 package com.project.TripHub.models;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-public class Guide {
-	@Id
+@Entity
+@Table(name="guides")
+public class Guide { 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-	@NotEmpty
+    private Long         id;
+    @NotEmpty
     private List<String> languages;
-   
-	@Size(min=9)
+
+    @Size(min=9)
     private Long license;
-	
-	
-    
-	@OneToMany(mappedBy="guide", fetch = FetchType.LAZY)
+
+    @Column(updatable = false)
+    private Date createdAt;
+    private Date updatedAt;
+
+    @OneToMany(mappedBy="guide", fetch = FetchType.LAZY)
     private List<Tour> guidedTours;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
+    public Guide() {
+    	
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<String> languages) {
+        this.languages = languages;
+    }
+
+    public Long getLicense() {
+        return license;
+    }
+
+    public void setLicense(Long license) {
+        this.license = license;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public List<Tour> getGuidedTours() {
+        return guidedTours;
+    }
+
+    public void setGuidedTours(List<Tour> guidedTours) {
+        this.guidedTours = guidedTours;
+    }
 }
